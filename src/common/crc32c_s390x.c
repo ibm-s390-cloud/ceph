36d26592b4b792fedc092e6adac8dcf74552c9b3
Aliaksei Makarau <aliaksei.makarau@ibm.com>
2024-03-06 10:46:42 +0100

    Reorganize to test

--- src/common/crc32c_s390x.c
+++ src/common/crc32c_s390x.c
@@ -16,26 +16,9 @@
 #define VX_ALIGN_MASK		(VX_ALIGNMENT - 1)
 
 /* Prototypes for functions in assembly files */
-unsigned int crc32_be_vgfm_16(unsigned int crc, const unsigned char *buf, size_t size);
-unsigned int crc32_le_vgfm_16(unsigned int crc, const unsigned char *buf, size_t size);
 unsigned int crc32c_be_vgfm_16(uint32_t crc, unsigned char const*buf, unsigned size);
-unsigned int crc32c_le_vgfm_16(uint32_t crc, unsigned char const*buf, unsigned size);
 
 /* Pure C implementations of CRC, one byte at a time */
-unsigned int crc32_be(unsigned int crc, const unsigned char *buf, size_t len) {
-	while (len--)
-		crc = crc32table_be[0][((crc >> 24) ^ *buf++) & 0xFF] ^ (crc << 8);
-	return crc;
-}
-
-/*
-unsigned int crc32c_be(unsigned int crc, const unsigned char *buf, size_t len) {
-	while (len--)
-		crc = crc32ctable_be[0][((crc >> 24) ^ *buf++) & 0xFF] ^ (crc << 8);
-	return crc;
-}
-*/
-
 unsigned int crc32c_be(uint32_t crc, unsigned char const *buf, unsigned len) {
 	if(buf != 0)
 		while (len--)
@@ -46,73 +29,34 @@ unsigned int crc32c_be(uint32_t crc, unsigned char const *buf, unsigned len) {
 	return crc;
 }
 
-unsigned int crc32_le(unsigned int crc, const unsigned char *buf, size_t len) {
-	crc = htole32(crc);
-	while (len--)
-		crc = crc32table_le[0][((crc >> 24) ^ *buf++) & 0xFF] ^ (crc << 8);
-	crc = le32toh(crc);
-	return crc;
-}
+unsigned int crc32c_be_vx(uint32_t crc, unsigned char const *data, unsigned datalen)
+{
+	unsigned long prealign, aligned, remaining;
 
-unsigned int crc32c_le(uint32_t crc, unsigned char const *buf, unsigned len) {
-	crc = htole32(crc);
-	if(buf != 0)
-		while (len--)
-			crc = crc32ctable_le[0][((crc >> 24) ^ *buf++) & 0xFF] ^ (crc << 8);
-	else
-		while (len--)
-			crc = crc32ctable_le[0][((crc >> 24)) & 0xFF] ^ (crc << 8);
-	crc = le32toh(crc);
-	return crc;
-}
+	if(data == 0)
+		return crc32c_be(crc, data, datalen);
 
-/*
- * DEFINE_CRC32_VX() - Define a CRC-32 function using the vector extension
- *
- * Creates a function to perform a particular CRC-32 computation. Depending
- * on the message buffer, the hardware-accelerated or software implementation
- * is used. Note that the message buffer is aligned to improve fetch
- * operations of VECTOR LOAD MULTIPLE instructions.
- *
- */
-#define DEFINE_CRC32_VX(___fname, ___crc32_vx, ___crc32_sw)                 \
-	unsigned int ___fname(uint32_t crc,                                 \
-			      unsigned char const *data,                    \
-			      unsigned datalen)                             \
-	{                                                                   \
-		unsigned long prealign, aligned, remaining;                 \
-                                                                            \
-		if(data == 0)                                               \
-			return ___crc32_sw(crc, data, datalen);             \
-		                                                            \
-		if(datalen < VX_MIN_LEN + VX_ALIGN_MASK)                    \
-			return ___crc32_sw(crc, data, datalen);             \
-		                                                            \
-		if ((unsigned long)data & VX_ALIGN_MASK) {                  \
-			prealign = VX_ALIGNMENT -                           \
-				   ((unsigned long)data & VX_ALIGN_MASK);   \
-			datalen -= prealign;                                \
-			crc = ___crc32_sw(crc, data, prealign);             \
-			data = data + prealign;                             \
-		}                                                           \
-		                                                            \
-		if (datalen < VX_MIN_LEN)                                   \
-			return ___crc32_sw(crc, data, datalen);             \
-		                                                            \
-		aligned = datalen & ~VX_ALIGN_MASK;                         \
-		remaining = datalen & VX_ALIGN_MASK;                        \
-		                                                            \
-		crc = ___crc32_vx(crc, data, aligned);                      \
-		data = data + aligned;                                      \
-		                                                            \
-		if (remaining)                                              \
-			crc = ___crc32_sw(crc, data, remaining);            \
-		                                                            \
-		return crc;                                                 \
+	if(datalen < VX_MIN_LEN + VX_ALIGN_MASK)
+		return crc32c_be(crc, data, datalen);
+
+	if ((unsigned long)data & VX_ALIGN_MASK) {
+		prealign = VX_ALIGNMENT - ((unsigned long)data & VX_ALIGN_MASK);
+		datalen -= prealign;
+		crc = crc32c_be(crc, data, prealign);
+		data = data + prealign;
 	}
 
-/* Main CRC-32 functions */
-//DEFINE_CRC32_VX(crc32_be_vx, crc32_be_vgfm_16, crc32_be)
-//DEFINE_CRC32_VX(crc32_le_vx, crc32_le_vgfm_16, crc32_le)
-DEFINE_CRC32_VX(crc32c_be_vx, crc32c_be_vgfm_16, crc32c_be)
-//DEFINE_CRC32_VX(crc32c_le_vx, crc32c_le_vgfm_16, crc32c_le)
+	if (datalen < VX_MIN_LEN)
+		return crc32c_be(crc, data, datalen);
+
+	aligned = datalen & ~VX_ALIGN_MASK;
+	remaining = datalen & VX_ALIGN_MASK;
+
+	crc = crc32c_be_vgfm_16(crc, data, aligned);
+	data = data + aligned;
+
+	if (remaining)
+		crc = crc32c_be(crc, data, remaining);
+
+	return crc;
+}
--- src/common/crc32c_s390x.h
+++ src/common/crc32c_s390x.h
@@ -7,19 +7,13 @@ extern "C" {
 #include <sys/types.h>
 #include <stdint.h>
 
-/* Portable implementations of CRC-32 (IEEE and Castagnoli), both
-   big-endian and little-endian variants. */
-unsigned int crc32_be(unsigned int, const unsigned char *, size_t);
-unsigned int crc32_le(unsigned int, const unsigned char *, size_t);
+/* Portable implementations of CRC-32 (IEEE and Castagnoli),
+   big-endian variant. */
 unsigned int crc32c_be(uint32_t, unsigned char const*, unsigned);
-unsigned int crc32c_le(uint32_t, unsigned char const*, unsigned);
 
 /* Hardware-accelerated versions of the above. It is up to the caller
    to detect the availability of vector facility and kernel support. */
-unsigned int crc32_be_vx(unsigned int, const unsigned char *, size_t);
-unsigned int crc32_le_vx(unsigned int, const unsigned char *, size_t);
 unsigned int crc32c_be_vx(uint32_t, unsigned char const*, unsigned);
-unsigned int crc32c_le_vx(uint32_t, unsigned char const*, unsigned);
 
 #ifdef __cplusplus
 }
