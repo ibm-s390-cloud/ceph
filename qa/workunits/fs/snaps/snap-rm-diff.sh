703e9bef1257cc00d148ad124aa2695925b93a12
Aliaksei Makarau <aliaksei.makarau@ibm.com>
2025-10-21 17:25:35 +0200

    mkdir -p foo/.snap/barsnap

--- qa/workunits/fs/snaps/snap-rm-diff.sh
+++ qa/workunits/fs/snaps/snap-rm-diff.sh
@@ -3,8 +3,8 @@
 wget -q http://download.ceph.com/qa/linux-2.6.33.tar.bz2
 mkdir foo
 cp linux* foo
-mkdir foo/.snap/barsnap
+mkdir -p foo/.snap/barsnap
 rm foo/linux*
 diff -q foo/.snap/barsnap/linux* linux* && echo "passed: files are identical"
-rmdir foo/.snap/barsnap
+rm -rf foo/.snap/barsnap
 echo OK
