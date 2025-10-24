fec513f7c64ecc53e1865b9717211d2439b709ba
Aliaksei Makarau <aliaksei.makarau@ibm.com>
2025-10-24 11:39:47 +0200

    mkdir -p

--- qa/workunits/fs/snaps/snaptest-1.sh
+++ qa/workunits/fs/snaps/snaptest-1.sh
@@ -6,11 +6,11 @@ echo 1 > file1
 echo 2 > file2
 echo 3 > file3
 [ -e file4 ] && rm file4
-mkdir .snap/snap1
+mkdir -p .snap/snap1
 echo 4 > file4
 now=`ls`
 then=`ls .snap/snap1`
-rmdir .snap/snap1
+rm -rf .snap/snap1
 if [ "$now" = "$then" ]; then
     echo live and snap contents are identical?
     false
@@ -20,9 +20,9 @@ fi
 echo 1 > file1
 echo 2 > file2
 echo 3 > file3
-mkdir .snap/snap1
+mkdir -p .snap/snap1
 echo 4 > file4
-rmdir .snap/snap1
+rm -rf .snap/snap1
 
 rm file?
 
